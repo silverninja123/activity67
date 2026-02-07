538d7190abedd62d8f1dd47eab2d27d0c063cc97
silverninja123 <goldencrumbs27@gmail.com>
2026-02-07 21:56:46 +0500

    Add files via upload

---
+++ index3.py
@@ -0,0 +1,7 @@
+number=input("Number: ")
+number=int(number)
+if number % 2 == 0:  
+    print(f"Your number {number} is an even integer")
+else:
+    print(f"Your number {number} is an odd integer")
+
